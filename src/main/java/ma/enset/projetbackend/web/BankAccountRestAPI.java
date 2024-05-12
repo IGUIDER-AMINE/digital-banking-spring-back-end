76f6e7bd7050c7fdb49b74b6c05c0e20548f6530
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-05-12 23:26:27 +0200

    partie2 finished

--- src/main/java/ma/enset/projetbackend/web/BankAccountRestAPI.java
+++ src/main/java/ma/enset/projetbackend/web/BankAccountRestAPI.java
@@ -1,5 +1,6 @@
 package ma.enset.projetbackend.web;
 
+import ma.enset.projetbackend.dtos.AccountHistoryDTO;
 import ma.enset.projetbackend.dtos.AccountOperationDTO;
 import ma.enset.projetbackend.dtos.BankAccountDTO;
 import ma.enset.projetbackend.exceptions.BankAccountNotFoundException;
@@ -29,15 +30,16 @@ public class BankAccountRestAPI {
     @GetMapping("/accounts/{accountId}/operations")
     public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
         return bankAccountService.accountHistory(accountId);
-    }/*
+    }
 
     @GetMapping("/accounts/{accountId}/pageOperations")
+    //http://localhost:8085/accounts/1360f676-2338-4c51-b941-4b009a5a2715/pageOperations?page=1
     public AccountHistoryDTO getAccountHistory(
             @PathVariable String accountId,
             @RequestParam(name="page",defaultValue = "0") int page,
             @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
         return bankAccountService.getAccountHistory(accountId,page,size);
-    }
+    }/*
     @PostMapping("/accounts/debit")
     public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
         this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
